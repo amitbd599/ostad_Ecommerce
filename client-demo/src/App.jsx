f8b0fae4d0b7e86a381528c3a88527c560978737
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-09-25 22:55:19 +0600

    add data

--- client-demo/src/App.jsx
+++ client-demo/src/App.jsx
@@ -5,6 +5,9 @@ import ScrollToTop from "react-scroll-to-top";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import AllProductPage from "./pages/AllProductPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CartPage from "./pages/CartPage";
+import CartPersonalPage from "./pages/CartPersonalPage";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/all-product' element={<AllProductPage />} />
+        <Route exact path='/product-details' element={<ProductDetailsPage />} />
+        <Route exact path='/cart' element={<CartPage />} />
+        <Route exact path='/cart-personal' element={<CartPersonalPage />} />
 
         <Route exact path='/register' element={<RegisterPage />} />
         <Route exact path='/login' element={<LoginPage />} />
